4170bd11d13281d9ef4d194972e5caba5891bf31
lucynguyen <ngu.nguyen@kyanon.digital>
2022-07-05 18:48:24 +0700

    Using takeEvery and takeLastest

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,13 +1,19 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { takeEvery } from "redux-saga/effects";
-import { increment } from "./counterSlice";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { incrementSaga, incrementSagaSuccess } from "./counterSlice";
 
-export function* log(action: PayloadAction) {
-  console.log(action);
+export function* handleIncreasementSaga(action: PayloadAction<number>) {
+  console.log('Waiting 1s');
+
+  yield delay(1000);
+
+  console.log('Waiting Done, dispatch action');
+
+  yield put(incrementSagaSuccess(action.payload));
 }
 
 export default function* counterSaga () {
   console.log('Counter Saga');
-
-  yield takeEvery(increment().type, log);
+  //  yield takeEvery(incrementSaga.toString(), handleIncreasementSaga);
+  yield takeLatest(incrementSaga.toString(), handleIncreasementSaga);
 }
