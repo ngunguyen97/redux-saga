336b1107724df96063ea924f950eb35c0ce708ae
lucynguyen <ngu.nguyen@kyanon.digital>
2022-07-12 19:13:22 +0700

    Implement student list UI

--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,5 +1,51 @@
-import * as React from 'react';
+import { Box, Typography, Button, makeStyles } from '@material-ui/core';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import React, { useEffect } from 'react';
+import StudentTable from '../components/StudentTable';
+import { selectStudentList, studentActions } from '../studentSlice';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    marginBottom: theme.spacing(4),
+  },
+}));
 
 export default function ListPage() {
-  return <div>Student List Page</div>;
+  const studentList = useAppSelector(selectStudentList);
+
+  const dispatch = useAppDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(
+      studentActions.fetchStudentList({
+        _page: 1,
+        _limit: 15,
+      })
+    );
+  }, [dispatch]);
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h4">Students</Typography>
+
+        <Button variant="contained" color="primary">
+          Add new student
+        </Button>
+      </Box>
+
+      {/* StudentTable */}
+      <StudentTable studentList={studentList} />
+
+      {/* Pagination */}
+    </Box>
+  );
 }
