707b41e8654b812fc0e40be3df8800f0744510bf
lucynguyen <ngu.nguyen@kyanon.digital>
2022-07-16 11:26:30 +0700

    Adding react-toastify lib

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { history } from 'utils';
 import App from './App';
 import { store } from './app/store';
-//import reportWebVitals from './reportWebVitals';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -17,6 +18,18 @@ ReactDOM.render(
         <CssBaseline />
         <App />
       </ConnectedRouter>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
